2922dc6b35cb6340ce09e69e7abdf68876032889
Jake <clayton@gmail.com>
2025-10-03 19:37:16 +0300

    Создание массива с данными о букетах

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+let bouquets = [
+    { id: 1, name: "Розы", price: 1500, image: "rose.jpg" },
+    { id: 2, name: "Тюльпаны", price: 1200, image: "tulip.jpg" },
+    { id: 3, name: "Пионы", price: 1800, image: "peony.jpg" },
+    { id: 4, name: "Лилии", price: 1600, image: "lily.jpg" },
+    { id: 5, name: "Хризантемы", price: 1100, image: "chrysanthemum.jpg" },
+    { id: 6, name: "Орхидеи", price: 2200, image: "orchid.jpg" },
+    { id: 7, name: "Герберы", price: 1300, image: "gerbera.jpg" },
+    { id: 8, name: "Ирисы", price: 1400, image: "iris.jpg" }
+];
 
+let orderItems = [];
